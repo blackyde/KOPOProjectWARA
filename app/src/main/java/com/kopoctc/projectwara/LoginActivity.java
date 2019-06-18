b182ab989ade2c6ecc4a1012f32366e50f0c0c73
zeke <blackyde@gmail.com>
2019-06-18 10:15:10 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/kopoctc/projectwara/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.kopoctc.projectwara;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button)findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                try {
+                    String result;
+                    CustomTask task = new CustomTask();
+                    EditText inputId = (EditText)findViewById(R.id.inputId);
+                    EditText inputPw = (EditText)findViewById(R.id.inputPw);
+                    String id = inputId.getText().toString();
+                    String pwd = inputPw.getText().toString();
+                    result = task.execute(id, pwd).get();
+                    Log.i("리턴 값", result);
+                    if(result.equals("true")) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "ID 나 PW 가 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+
+    class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("http://35.221.189.227/login.jsp");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "id="+strings[0]+"&pwd="+strings[1];
+                osw.write(sendMsg);
+                osw.flush();
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return receiveMsg;
+        }
+    }
+
+}

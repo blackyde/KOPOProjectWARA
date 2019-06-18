428ab8909ad9094fcab12472f9293002323cf821
zeke <blackyde@gmail.com>
2019-06-18 13:25:21 +0900

    MainActivity 추가 (개발중)

--- app/src/main/java/com/kopoctc/projectwara/MainActivity.java
+++ app/src/main/java/com/kopoctc/projectwara/MainActivity.java
@@ -2,6 +2,9 @@ package com.kopoctc.projectwara;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMain = (Button)findViewById(R.id.btnMain);
+
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "개발중", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

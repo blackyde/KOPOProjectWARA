4c0f4eb241e7930a71a2efdd11935bb7b6a1c4f9
zeke <blackyde@gmail.com>
2019-06-21 10:57:23 +0900

    apk 추출

--- app/src/main/java/com/kopoctc/projectwara/LoginActivity.java
+++ app/src/main/java/com/kopoctc/projectwara/LoginActivity.java
@@ -9,7 +9,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 //import android.util.Base64;
 import android.util.Log;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
--- app/src/main/java/com/kopoctc/projectwara/SignActivity.java
+++ app/src/main/java/com/kopoctc/projectwara/SignActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
